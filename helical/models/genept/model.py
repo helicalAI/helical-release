e7d7af05191dd64d2e5a2de2a783434dee11fb4f
maxiallard <>
2024-12-19 19:51:26 +0100

    first try at functioning genept

--- helical/models/genept/model.py
+++ helical/models/genept/model.py
@@ -4,17 +4,20 @@ from pathlib import Path
 import numpy as np
 from anndata import AnnData
 from helical.utils.downloader import Downloader
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM,AutoModel
 from helical.models.geneformer.geneformer_utils import get_embs,quant_layers
 from helical.models.geneformer.geneformer_tokenizer import TranscriptomeTokenizer
 from helical.models.genept.genept_config import GenePTConfig
-from helical.utils.mapping import map_gene_symbols_to_ensembl_ids
+from helical.utils.mapping import map_ensembl_ids_to_gene_symbols
 from datasets import Dataset
 from typing import Optional
 from accelerate import Accelerator
 import logging
 import scanpy as sc
 import torch
+import json
+import pandas as pd
+from tqdm import tqdm 
 logger = logging.getLogger(__name__)
 
 model_preamble= '''
@@ -65,11 +68,99 @@ class GenePT(HelicalRNAModel):
 
         # model_id = "CohereForAI/c4ai-command-r7b-12-2024"
         self.tokenizer = AutoTokenizer.from_pretrained(self.config["model_name"])
-        self.model = AutoModelForCausalLM.from_pretrained(self.config["model_name"],torch_dtype=torch.bfloat16, device_map="auto").to(self.config["device"])
+        # self.model = AutoModelForCausalLM.from_pretrained(self.config["model_name"],torch_dtype=torch.bfloat16, device_map="auto").to(self.config["device"])
+        self.model = AutoModel.from_pretrained(self.config["model_name"],torch_dtype=torch.bfloat16, device_map="auto").to(self.config["device"])
 
+        with open("genept_embeddings.json","r") as f:
+            self.embeddings = json.load(f)
+    
         self.model.post_init()
         logger.info("GenePT initialized successfully.")
         
+    def process_data_gen(self, 
+                     adata: AnnData,  
+                     gene_names: str = "index", 
+                     output_path: Optional[str] = None,
+                     use_raw_counts: bool = True,
+                     ) -> Dataset:   
+        """
+        Processes the data for the Geneformer model.
+
+        Parameters
+        ----------
+        adata : AnnData
+            The AnnData object containing the data to be processed. Geneformer uses Ensembl IDs to identify genes 
+            and currently supports only human genes. If the AnnData object already has an 'ensembl_id' column, 
+            the mapping step can be skipped.
+        gene_names : str, optional, default="index"
+            The column in `adata.var` that contains the gene names. If set to a value other than "ensembl_id", 
+            the gene symbols in that column will be mapped to Ensembl IDs using the 'pyensembl' package, 
+            which retrieves mappings from the Ensembl FTP server and loads them into a local database.
+            - If set to "index", the index of the AnnData object will be used and mapped to Ensembl IDs.
+            - If set to "ensembl_id", no mapping will occur.
+            Special case:
+                If the index of `adata` already contains Ensembl IDs, setting this to "index" will result in 
+                invalid mappings. In such cases, create a new column containing Ensembl IDs and pass "ensembl_id" 
+                as the value of `gene_names`.
+        output_path : str, optional, default=None
+            If specified, saves the tokenized dataset to the given output path.
+        use_raw_counts : bool, optional, default=True
+            Determines whether raw counts should be used.
+
+        Returns
+        -------
+        Dataset
+            The tokenized dataset in the form of a Huggingface Dataset object.
+        """
+
+        self.ensure_rna_data_validity(adata, gene_names, use_raw_counts)
+
+        # map gene symbols to ensemble ids if provided
+        if gene_names == "ensembl_id":
+            if (adata.var[gene_names].str.startswith("ENS").all()) or (adata.var[gene_names].str.startswith("None").any()):
+                message = "It seems an anndata with 'ensemble ids' and/or 'None' was passed. " \
+                "Please set gene_names='ensembl_id' and remove 'None's to skip mapping."
+                logger.info(message)
+                raise ValueError(message)
+            adata = map_ensembl_ids_to_gene_symbols(adata, gene_names)
+
+
+        sc.pp.highly_variable_genes(adata,n_top_genes=100,flavor='seurat_v3')
+
+        genes_names = adata.var_names[adata.var['highly_variable']].tolist()
+
+        adata = adata[:,genes_names]
+        prompts = []
+        for gene in genes_names:
+            conversation = [
+            {"role": "system", "content": model_preamble},
+            {"role": "user", "content": "Tell me about gene {} and its pathways.".format(gene)}
+            ]
+            prompts.append(conversation)
+
+        dataset = Dataset.from_dict({"genes": prompts})
+        dataset = dataset.map(lambda x: {"formatted_chat": self.tokenizer.apply_chat_template(x["genes"],max_length=4096, tokenize=True, add_generation_prompt=True,return_tensors="pt",truncation=True,padding='max_length')}).with_format("torch")
+        print(dataset['formatted_chat'][0])
+        # input_ids = self.tokenizer.apply_chat_template(prompts, tokenize=True, add_generation_prompt=True, return_tensors="pt",truncation=True)
+        all_outputs = []
+        for chat in dataset['formatted_chat'].to(self.config["device"]):
+            gen_outputs = self.model.generate(
+                inputs=chat,
+                max_new_tokens=512,
+                do_sample=True,
+                temperature=0.3,
+                # skip_special_tokens=True,
+                output_hidden_states=True,
+                return_dict_in_generate=True
+
+            )
+            ## get the last hidden state of the last token for the last layer of the model. 
+            ##See https://huggingface.co/docs/transformers/v4.47.1/en/internal/generation_utils#transformers.generation.GenerateDecoderOnlyOutput
+            gen_outputs = gen_outputs.hidden_states[-1][-1]
+            all_outputs.append(gen_outputs)
+        return adata, all_outputs
+    
+
     def process_data(self, 
                      adata: AnnData,  
                      gene_names: str = "index", 
@@ -109,52 +200,48 @@ class GenePT(HelicalRNAModel):
         self.ensure_rna_data_validity(adata, gene_names, use_raw_counts)
 
         # map gene symbols to ensemble ids if provided
-        if gene_names != "ensembl_id":
+        if gene_names == "ensembl_id":
             if (adata.var[gene_names].str.startswith("ENS").all()) or (adata.var[gene_names].str.startswith("None").any()):
                 message = "It seems an anndata with 'ensemble ids' and/or 'None' was passed. " \
                 "Please set gene_names='ensembl_id' and remove 'None's to skip mapping."
                 logger.info(message)
                 raise ValueError(message)
-            adata = map_gene_symbols_to_ensembl_ids(adata, gene_names)
+            adata = map_ensembl_ids_to_gene_symbols(adata, gene_names)
 
 
-        sc.pp.highly_variable_genes(adata,n_top_genes=100,flavor='seurat_v3')
+        sc.pp.highly_variable_genes(adata,n_top_genes=1000,flavor='seurat_v3')
+        sc.pp.normalize_total(adata,target_sum=1e4)
+        sc.pp.log1p(adata)
 
         genes_names = adata.var_names[adata.var['highly_variable']].tolist()
 
         adata = adata[:,genes_names]
-        prompts = []
-        for gene in genes_names:
-            conversation = [
-            {"role": "system", "content": model_preamble},
-            {"role": "user", "content": "Tell me about gene {} and its pathways.".format(gene)}
-            ]
-            prompts.append(conversation)
-
-        dataset = Dataset.from_dict({"genes": prompts})
-        dataset = dataset.map(lambda x: {"formatted_chat": self.tokenizer.apply_chat_template(x["genes"],max_length=4096, tokenize=True, add_generation_prompt=True,return_tensors="pt",truncation=True,padding='max_length')}).with_format("torch")
-        print(dataset['formatted_chat'][0])
         # input_ids = self.tokenizer.apply_chat_template(prompts, tokenize=True, add_generation_prompt=True, return_tensors="pt",truncation=True)
-        all_outputs = []
-        for chat in dataset['formatted_chat'].to(self.config["device"]):
-            gen_outputs = self.model.generate(
-                inputs=chat,
-                max_new_tokens=512,
-                do_sample=True,
-                temperature=0.3,
-                # skip_special_tokens=True,
-                output_hidden_states=True,
-                return_dict_in_generate=True
+        
+        return adata
 
-            )
-
-            ## get the last hidden state of the last token for the last layer of the model. 
-            ##See https://huggingface.co/docs/transformers/v4.47.1/en/internal/generation_utils#transformers.generation.GenerateDecoderOnlyOutput
-            gen_outputs = gen_outputs.hidden_states[-1][-1]
-            all_outputs.append(gen_outputs)
-        return adata, all_outputs
-
-    def get_embeddings(self, dataset: Dataset) -> np.array:
+    def create_embeddings_from_texts(self, text="./NCBI_cleaned_summary_of_genes.json") -> dict:
+        batch_size = 2
+        embeddings = []
+        all_texts = json.load(open(text))
+        df = pd.DataFrame([all_texts]).T
+        df.columns = ["text"]
+        for batch in tqdm(range(0,len(df),batch_size)):
+            input_ids = self.tokenizer(df['text'][batch:batch+batch_size].values.tolist(),return_tensors="pt",max_length=1000,padding=True,truncation=True).to(config["device"])
+            res = self.model(input_ids['input_ids'],attention_mask=input_ids['attention_mask'],output_hidden_states=True)
+            embeddings.append(res.last_hidden_state[:,-1].float().cpu().detach().numpy())
+            del input_ids
+            del res
+        all_embs = np.vstack(embeddings)
+        # np.save("genept_embeddings.npy",all_embs)
+        df['embeddings'] = all_embs.tolist()
+        dictionary = df.to_dict(orient="index")
+        with open("genept_embeddings.json","w") as f:
+            json.dump(dictionary,f)
+        self.mapping = dictionary
+        return self.mapping
+        
+    def get_embeddings(self, dataset: AnnData) -> np.array:
         """Gets the gene embeddings from the Geneformer model   
 
         Parameters
@@ -169,38 +256,18 @@ class GenePT(HelicalRNAModel):
         """
         logger.info(f"Inference started:")
         # Generate a response 
-        prompts = []
-        for gene in adata.var.index:
-            conversation = [
-            {"role": "system", "content": model_preamble},
-            {"role": "user", "content": "Tell me about gene {} and its pathways.".format(gene)}
-            ]
-            prompts.append(conversation)
-
-        input_ids = self.tokenizer.apply_chat_template(prompts, tokenize=True, add_generation_prompt=True, return_tensors="pt")
-
-        gen_tokens = model.generate(
-
-            input_ids,
-
-            max_new_tokens=512,
-
-            do_sample=True,
-
-            temperature=0.3,
-
-            skip_special_tokens=True,
-
-        )
-        embeddings = get_embs(
-            self.model,
-            dataset,
-            self.emb_mode,
-            self.layer_to_quant,
-            self.pad_token_id,
-            self.forward_batch_size,
-            self.gene_token_dict,
-            self.device
-        ).cpu().detach().numpy()
-
+        raw_embeddings = dataset.var_names
+        weights = []
+        gene_list = []
+        for i,emb in enumerate(raw_embeddings):
+            gene = self.embeddings.get(emb,None)
+            if gene is not None:
+                weights.append(gene['embeddings'])
+                gene_list.append(emb)
+            else:
+                logger.info("Couln't find {} in embeddings".format(emb))
+        weights = torch.Tensor(weights)
+        embeddings = torch.matmul(torch.Tensor(dataset[:,gene_list].X.todense()),weights)
+        embeddings = (embeddings/(np.linalg.norm(embeddings,axis=1)).reshape(-1,1))
+        # embeddings = torch.nn.functional.normalize(embeddings, p=2, dim=1) --> Same as above
         return embeddings
